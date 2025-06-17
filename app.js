f763ce22925683cdf09e18df6a73661929ae21d9
Rogersovich <53912205+rogersovich@users.noreply.github.com>
2025-06-17 15:17:44 +0700

    Add initial project setup with Express.js

---
+++ app.js
@@ -0,0 +1,15 @@
+// app.js
+const express = require('express');
+const app = express();
+const port = 3000; // You can choose any available port
+
+app.get('/', (req, res) => {
+  res.json({
+    message: "Hello World",
+    name: "Dimas Roger W", 
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Express app listening at http://localhost:${port}`);
+});
